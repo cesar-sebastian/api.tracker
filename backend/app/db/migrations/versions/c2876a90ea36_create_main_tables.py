64af1eac7aabee40a92624a10ec77a70f7caa87d
Cesar <cesar.carrazana@acilia.es>
2022-01-11 10:27:14 -0300

    Add create tables to migration

--- backend/app/db/migrations/versions/c2876a90ea36_create_main_tables.py
+++ backend/app/db/migrations/versions/c2876a90ea36_create_main_tables.py
@@ -15,11 +15,27 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+def create_cleanings_table() -> None:
+    op.create_table(
+        "property",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.Text, nullable=False, index=True),
+        sa.Column("lat", sa.Float, nullable=True),
+        sa.Column("lon", sa.Float, nullable=True),
+    )
+    op.create_table(
+        "tracker",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("lat", sa.Float, nullable=False, index=True),
+        sa.Column("lon", sa.Float, nullable=True),
+    )
+
 
 def upgrade() -> None:
-    pass
+    create_cleanings_table()
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table("property")
+    op.drop_table("tracker")
 
