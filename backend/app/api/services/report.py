5d06518a9c580d04d6b8a38f9bae03bf2b11625b
Cesar <cesar.carrazana@acilia.es>
2022-01-15 21:47:31 -0300

    Enhancement

--- backend/app/api/services/report.py
+++ backend/app/api/services/report.py
@@ -1,11 +1,9 @@
 from typing import List
 from app.db.repositories.tracker import TrackerRepository
 from app.db.repositories.property import PropertyRepository
-from app.api.dependencies.database import get_repository
 from app.models.report import Report
-from fastapi import Depends
 from app.api.utils import coordinates
-import datetime
+from datetime import datetime, timedelta
 
 class ReportService():
 
@@ -15,8 +13,8 @@ class ReportService():
             tracker_repo: TrackerRepository
         ) -> List[Report]:
 
-        date_from = datetime.datetime.strptime(day, '%Y-%m-%d')
-        date_to = date_from + datetime.timedelta(days=1)
+        date_from = datetime.strptime(day, '%Y-%m-%d')
+        date_to = date_from + timedelta(days=1)
         properties = await property_repo.fetch_all()        
         trackers = await tracker_repo.fetch_trackers(date_from, date_to)
         
@@ -26,16 +24,16 @@ class ReportService():
             trackers_time = []
             for tracker in trackers:          
                 if coordinates.isProperty((property.lat, property.lon), (tracker.lat, tracker.lon)):
-                    trackers_time.append(property.date)
-                    trackers.pop(tracker)
+                    trackers_time.append(tracker.created_at)
+                    #trackers.pop(tracker)
             
             if len(trackers_time) > 0:
                 property_traker[property.name] = trackers_time
 
         report = []
         for name in property_traker:
-            time = property_traker[name][-1] - property_traker[name][0]
-            report.append(Report(name, time, day))
+            time = (property_traker[name][-1] - property_traker[name][0]) / timedelta(milliseconds=1)            
+            report.append(Report(property=name, time=time, day=day))
         
         return report
 
